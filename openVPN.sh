8bdd5e0fb550f8690547a5b77a09e3dd91076532
cheng <chengff11@gmail.com>
2025-09-10 20:35:45 +0800

    Update openVPN.sh
    usage: ./openVPN.sh your_email webadress

--- openVPN.sh
+++ openVPN.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <email_address> <vpn_address>"
+    exit 1
+fi
+
+# Assign the input variables
+EMAIL_ADDRESS=$1
+VPN_ADDRESS=$2
+
 # 开放端口
 sudo iptables -I INPUT -p tcp --dport 8443 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
@@ -9,4 +21,4 @@ systemctl start squid
 nohup nghttpx >/dev/null 2>&1 &
 
 # 更新证书
-sudo certbot certonly --standalone --email 1105470619@qq.com -d cheng.chengff.com --agree-tos
+sudo certbot certonly --standalone --email $EMAIL_ADDRESS -d $VPN_ADDRESS --agree-tos
